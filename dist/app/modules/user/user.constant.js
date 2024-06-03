c3cf007d55e94e5e3a9d8dacebdb7e379c72791d
Misbahur Rahman <misbahurbd@gmail.com>
2024-06-03 07:32:32 +0600

    remove dist from git

--- dist/app/modules/user/user.constant.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userSearchFields = exports.userQueryFields = void 0;
-exports.userQueryFields = ["searchTerm"];
-exports.userSearchFields = ["name", "email", "mobile", "username"];
--- dist/utils/send-response.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendResponse = void 0;
-const sendResponse = (res, jsonObj) => {
-    res.status(jsonObj.statusCode || 200).json(Object.assign({ statusCode: jsonObj.statusCode || 200, success: jsonObj.success || true }, jsonObj));
-};
-exports.sendResponse = sendResponse;
